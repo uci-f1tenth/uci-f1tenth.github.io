8a99bf9b085b9ea4c23b9a23318531ce1c54fe97
tadahiroueta <tadahiroueta@gmail.com>
2025-03-03 14:00:08 -0800

    feat: header, footer, and home page (web view)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,20 +8,20 @@ export default function Home() {
         {/* opaque filter */}
         <div className="absolute inset-0 bg-cyan-900 opacity-60" />
         {/* text */}
-        <div className="w-full z-10 flex flex-col gap-2.5 items-center px-5 py-2.5">
-          <h1 className="font-black italic text-6xl tracking-wider text-right text-white">F1TENTH<br />@UCI</h1>
-          <p className="w-full font-semibold text-white">Building fast, <a href="https://www.youtube.com/watch?v=lbP01VaWrVU">small-scale autonomous racing cars</a> is an exciting challenge that we hope to conquer.</p>
+        <div className="w-full md:w-[40rem] z-10 flex flex-col gap-2.5 items-center px-5 py-2.5">
+          <h1 className="font-black italic text-6xl tracking-wider text-right text-white">F1TENTH<br className="md:hidden" />@UCI</h1>
+          <p className="w-full font-semibold md:text-center text-white">Building fast, <a href="https://www.youtube.com/watch?v=lbP01VaWrVU">small-scale autonomous racing cars</a> is an exciting challenge that we hope to conquer.</p>
         </div>
       </div>
       {/* about us */}
-      <div className="relative w-full flex flex-col gap-10 justify-center px-5 pt-44 pb-16">
+      <div className="relative w-full flex flex-col gap-10 justify-center px-5 md:px-48 pt-44 md:pt-32 pb-16 md:pb-32">
         {/* background picture */}
-        <img src="/assets/IMG_0322.webp" alt="team" className="absolute -z-10 h-full scale-125 overflow-visible object-cover bottom-15 left-60" />
+        <img src="/assets/IMG_0322.webp" alt="team" className="absolute -z-10 md:w-full h-full scale-125 md:scale-none overflow-visible object-cover bottom-15 md:bottom-auto left-60 md:left-0" />
         {/* opaque filter */}
         <div className="absolute inset-0 bg-black opacity-70" />
         {/* text */}
         <h2 className="z-10 font-black text-5xl text-white">About Us</h2>
-        <p className="z-10 w-full font-semibold text-white">
+        <p className="z-10 w-full md:w-[26rem] font-semibold md:font-normal text-white">
           F1Tenth is an international community of autonomous systems enthusiasts. 
           We are a student-run club aimed at developing machine learning algorithms 
           and building hardware. Our goal is to learn, innovate, and build these 
@@ -34,18 +34,18 @@ export default function Home() {
         </p>
       </div>
       {/* fields */}
-      <div className="w-full flex flex-col gap-16 items-center py-16 bg-gray-900">
-        <h2 className="font-black text-5xl text-center uppercase text-white">TWO FIELDS ONE GOAL</h2>
+      <div className="w-full flex flex-col gap-16 md:gap-8 items-center py-16 bg-gray-900">
+        <h2 className="font-black text-5xl text-center uppercase text-white">TWO FIELDS <br className="hidden md:block" />ONE GOAL</h2>
         {/* windows */}
-        <div className="w-full flex flex-col gap-16 px-20">
-          <div className="w-full flex flex-col gap-5 items-center">
+        <div className="w-full md:w-[60rem] flex flex-col md:flex-row gap-16 md:justify-between px-20 md:px-0">
+          <div className="w-full md:w-80 flex flex-col gap-5 items-center">
             <img src="/assets/IMG_0064.webp" alt="car" className="w-full aspect-square object-cover rounded-full" />
             <h3 className="font-extrabold text-4xl text-white uppercase">ROBOTICS</h3>
             <Link to="/robotics">
               <h4 className="p-2.5 font-black text-xl uppercase bg-white text-gray-900">TEAM GOGGINS</h4>
             </Link>
           </div>
-          <div className="w-full flex flex-col gap-5 items-center">
+          <div className="w-full md:w-80 flex flex-col gap-5 items-center">
             <img src="/assets/IMG_0394.webp" alt="car" className="w-full aspect-square object-cover rounded-full" />
             <h3 className="font-extrabold text-4xl text-white uppercase">AI/RL</h3>
             <Link to="/robotics">
@@ -55,10 +55,10 @@ export default function Home() {
         </div>
       </div>
       {/* gallery */}
-      <div className="w-full flex flex-col items-center">
-        <img src="/assets/IMG_0382.webp" alt="simulation" className="w-full" />
-        <img src="/assets/2d740d7e110c66f2bda244443d41a0c5.webp" alt="computer surgery" className="w-full" />
-        <img src="/assets/IMG_0061.webp" alt="card" className="w-full" />
+      <div className="w-full md:h-[34rem] flex flex-col md:flex-row items-center">
+        <img src="/assets/IMG_0382.webp" alt="simulation" className="w-full md:object-cover md:w-1/3 md:h-full" />
+        <img src="/assets/2d740d7e110c66f2bda244443d41a0c5.webp" alt="computer surgery" className="w-full md:object-cover md:w-1/3 md:h-full" />
+        <img src="/assets/IMG_0061.webp" alt="card" className="w-full md:object-cover md:w-1/3 md:h-full" />
       </div>
       {/* contact */}
       <div className="w-full flex flex-col items-center px-5 py-32 bg-[url(/assets/Screenshot_2025-02-05_at_6.03.01_PM.webp)] bg-cover bg-center">
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,11 +8,33 @@ function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 w-full z-10 flex flex-col gap-2.5 items-end px-5 py-2.5">
+    <header className="sticky top-0 w-full z-10 flex flex-col gap-2.5 items-end px-5 md:px-10 py-2.5">
       {/* always visible content */}
       <div className="w-full flex justify-between items-center">
         <img src="/assets/F1TenthUCIlogo.png" alt="logo" className="w-[7.5rem]" />
-        <Bars3Icon onClick={ () => { setIsMenuOpen(!isMenuOpen); }} className="w-12 text-white" />
+        <Bars3Icon onClick={ () => { setIsMenuOpen(!isMenuOpen); }} className="w-12 text-white md:hidden" />
+        <ul className="flex gap-2 5 items-center p-2 5">
+          <li>
+            <Link to="/">
+              <p className="font-semibold uppercase text-white">HOME</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/robotics">
+              <p className="font-semibold uppercase text-white">ROBOTICS</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/ai-rl">
+              <p className="font-semibold uppercase text-white">AI/RL</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/mailto:f1tenth@uci.edu">
+              <p className="font-semibold uppercase text-white">CONTACT US</p>
+            </Link>
+          </li>
+        </ul>
       </div>
       <AnimatePresence>
         { isMenuOpen && (
@@ -52,8 +74,8 @@ function Header() {
 
 function Footer() {
   return (
-    <footer className="w-full flex flex-col gap-10 items-center py-12 bg-neutral-800">
-      <h3 className="font-black text-2xl text-white">JOIN OUR SERVER</h3>
+    <footer className="w-full flex flex-col gap-10 md:gap-5 items-center py-12 bg-neutral-800">
+      <h3 className="font-black text-2xl md:text-5xl text-white">JOIN OUR SERVER</h3>
       <Link to="https://discord.gg/KWENJaPCMK">
         <img src="https://support.discord.com/hc/user_images/PRywUXcqg0v5DD6s7C3LyQ.jpeg" alt="discord logo" className="w-20 rounded-2xl" />
       </Link>
